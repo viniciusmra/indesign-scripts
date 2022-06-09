4b79b577c9cf7ca978c850903d53544f836ece00
Vinícius Alves <viniciusmra@gmail.com>
2022-06-09 12:33:50 -0300

    comentários e implementação da interface

--- import-word-docs.jsx
+++ import-word-docs.jsx
@@ -1,65 +1,68 @@
 /*
     Autor: Vinicius Alves - viniciusmra@gmail.com
     Data: 2021
-    Descrição: Este script 
-
-
-    TO-DO:
-    - Quando os arquivos importados forem colocados no meio de uma sequência linkada, tem que haver uma forma de quebrar esse link
-    - dar a opção de autoflow
-    - organizar a interface
-    - comentar o código
-
-
-    
+    Última atualização: jun/2022
+    Descrição:  
+    Este script tem a função de importar e linkar múltiplos arquivos do word (.doc e .docx) dentro do inDesign.
+    Por padrão os documentos são importados, cada uma e uma página e depois linkados uns aos outros.
+    No entanto, existe a opção de autoflow, que faz com que os documentos importados ocupem o número de páginas necessárias
+    para que não ocorra "overset text". Essa opção pode ocasionar problemas em alguns tipos de documentos.
 */
 
-
-//@include showProps.jsx;
-
-
 var currentDoc = app.activeDocument;
-
-var firstPageNum = currentDoc.pages.length-1; // TO DO: Fazer a UI perguntar isso 
 var nPages = currentDoc.pages.length;
 var pages = currentDoc.pages.everyItem().getElements();
 var pageList = currentDoc.pages.everyItem().name;
-var currentPage = currentDoc.pages.item(firstPageNum);
+var currentPage = currentDoc.pages.item(0);
 var x = currentPage.marginPreferences.left;
 var y = currentPage.marginPreferences.top;
 var files;
 
-//alert(pageList[0])
+//INTERFACE
+var myWindow = new Window ("dialog", "Importar arquivos"); // Cria a janela da interface
 
-//showProps(currentDoc.pages.everyItem())
-// Cria a janela da interface
-var myWindow = new Window ("dialog", "Importar arquivos");
+// Grupo 1
+var group1 = myWindow.add('group {alignment:["fill","fill"]}')
+group1.orientation = "row";
+group1.add("statictext", undefined, "Selecione os arquivos que deseja importar");
+var selectButton = group1.add ('button {text: "Selecionar"}');
 
-//
-var text1 = myWindow.add("statictext", undefined, "Selecione os arquivos que deseja importar");
-var selectButton = myWindow.add ('button {text: "Selecionar"}');
-
-var myDropdown = myWindow.add ("dropdownlist", undefined, pageList);
-myDropdown.selection = nPages-1;
-
+// ListBox
+myWindow.add("statictext", undefined, "Arquivos selecionados:").alignment = "left";
 var fileListBox = myWindow.add("listbox", undefined);
-fileListBox.preferredSize.width = 540;
+fileListBox.preferredSize.width = 500;
 fileListBox.preferredSize.height = 300;
 
-var moveUpButton = myWindow.add ('button {text: "Mover para cima"}');
-var moveDownButton = myWindow.add ('button {text: "Mover para baixo"}');
-var delButton = myWindow.add ('button {text: "Remover arquivo"}');
+// Grupo 2
+var group2 = myWindow.add('group {alignment:["center","fill"]}')
+group2.orientation = "row";
+var moveUpButton = group2.add ('button {text: "Mover para cima"}');
+var moveDownButton = group2.add ('button {text: "Mover para baixo"}');
+var delButton = group2.add ('button {text: "Remover arquivo"}');
 moveDownButton.enabled = false;
 moveUpButton.enabled = false;
 delButton.enabled = false;
 
-var okButton = myWindow.add ("button", undefined, "Importar", {name: "ok"});
+// Grupo 3
+var group3 = myWindow.add('group {alignment:["left","fill"]}')
+group3.orientation = "row";
+group3.add("statictext", undefined, "Inserir arquivos após a página: ");
+var dropdownList = group3.add ("dropdownlist", undefined, pageList);
+dropdownList.selection = nPages-1;
+
+//CheckBox
+var checkBox = myWindow.add ("checkbox", undefined, "Fazer \"autoFlow\"");
+checkBox.alignment = "left";
+
+// Grupo 4
+var group4 = myWindow.add('group {alignment:["center","fill"]}')
+group4.orientation = "row";
+var okButton = group4.add ("button", undefined, "Importar", {name: "ok"});
 okButton.enabled = false;
-myWindow.add ("button", undefined, "Cancelar", {name: "cancel"});
+group4.add ("button", undefined, "Cancelar", {name: "cancel"});
 
-
-
-
+// Funções
+// Abre a caixa de diálogo para selecionar os arquivos
 selectButton.onClick = function () {
     if(files == null){
         files = File.openDialog("Escolha os arquivos", "*.doc;*.docx", true);
@@ -72,6 +75,7 @@ selectButton.onClick = function () {
     updateListBox(files,fileListBox);
 }
 
+// Move o item selecionado na listBox para cima
 moveUpButton.onClick = function () {
     var index = fileListBox.selection.index;
     if(moveUp(files, fileListBox.selection.index)){
@@ -79,7 +83,7 @@ moveUpButton.onClick = function () {
         fileListBox.selection = index - 1;
     }
 }
-
+// Move o item selecionado na listBox para baixo
 moveDownButton.onClick = function () {
     var index = fileListBox.selection.index;
     if(moveDown(files, fileListBox.selection.index)){
@@ -88,13 +92,14 @@ moveDownButton.onClick = function () {
     }   
 }
 
+// Remove o item selecionado da listBox
 delButton.onClick = function () {
-    
     var index = fileListBox.selection.index;
     
     if(fileListBox.selection != null){
         files.splice(index,1);
     }
+
     updateListBox(files,fileListBox);
 
     if(index == files.length){
@@ -104,18 +109,18 @@ delButton.onClick = function () {
     }
 }
 
+// Roda o script caso o botão importar tenha sido pressionado
 if(myWindow.show() == 1){
+    var pageNum = dropdownList.selection.index;
     if(files != null){
         if(files.length > 0){
-            //currentDoc.pages.add(LocationOptions.AFTER, pages[myDropdown.selection.index])
-            importFiles(files, myDropdown.selection.index);
-            //linkFrames(firstPageNum)
-        }
+            importFiles(files, pageNum);
+            linkFrames(pageNum + 1,files.length)
+        } 
     }
 }
 
-
-
+// Move o elemento no índice "index" para a posição "index - 1" caso ela exista
 function moveUp(arr, index){
     if(index > 0){
         var temp = arr[index-1];
@@ -126,6 +131,7 @@ function moveUp(arr, index){
     return false
 }
 
+// Move o elemento no índice "index" para a posição "index + 1" caso ela exista
 function moveDown(arr, index){
     if(index < arr.length-1){
         var temp = arr[index+1];
@@ -136,6 +142,7 @@ function moveDown(arr, index){
     return false
 }
 
+// Remove todos os elementos da listBox e coloca novos elementos
 function updateListBox(items, list){
     list.removeAll();
     if(items.length > 0){
@@ -156,100 +163,53 @@ function updateListBox(items, list){
     }
 }
 
-
-
-
-
-// Abre a caixa de dialogo que seleciona os arquivos
-function getFilesList(){
-    return File.openDialog("Please choose a file", "*.doc;*.docx", true);
-}
-
 // Importa os arquivos e coloca cada um em uma página
 function importFiles(files, pageNum){
     var currentPage = pages[pageNum]
     for(var i = 0; i < files.length; i++){
         currentDoc.pages.add(LocationOptions.AFTER, currentPage)
-        //if(pageNum + i > currentDoc.pages.length-1){
-        //    currentDoc.pages.add();
-        //}
-        //var currentPage = currentDoc.pages.item(pageNum + i);
         currentPage = currentDoc.pages.item(pageNum + 1);
         currentPage.place(File(files[i]), [x,y], undefined, false,false)[0];
         pageNum++;
 
     }
 }
-// junta as text frames, mas não fazer o auto flow
-function linkFrames(pageNum){
-    var allPages = currentDoc.pages;
-    var currentFrame = allPages[pageNum].textFrames[0];
 
-    for(var i = 1; i < allPages.length; i++){
-        if(pageNum + i >= allPages.length){
-            break;
-        } else{
-            currentFrame.nextTextFrame = allPages[pageNum + i].textFrames[0];
-            currentFrame = allPages[pageNum + i].textFrames[0];
+// junta os Text Frames e faz "autoflow" caso a checkBox tenha sido marcada pelo usuário
+function linkFrames(pageNum, nFiles){
+    var allPages = currentDoc.pages;                        // Cria um lista com todas as páginas do documento
+    var currentFrame = allPages[pageNum].textFrames[0];     // Define o Frame atual como sendo o primeiro Frame da página atual
+    
+    // Linka os arquivos recem importados
+    for(var i = 0; i < nFiles-1; i++){
+            pageNum++;
+            currentFrame.nextTextFrame = allPages[pageNum].textFrames[0];
+            currentFrame = allPages[pageNum].textFrames[0];
+    }
+    // Verifica se o usuário marcou o checkBox e executa o autoflow
+    if(checkBox.value == 1){
+        var currentPage = currentDoc.pages.item(pageNum);
+        while(currentFrame.overflows){
+            currentDoc.pages.add(LocationOptions.AFTER, currentPage);   // Cria uma página depois da página atual
+            nextPage = allPages[++pageNum];                             // Define a próxima página
+            nextFrame = addFrame(nextPage);                             // Adiciona um novo Frame a página, o nextFrame
+            currentFrame.nextTextFrame = nextFrame;                     // Linka o Frame da página atual com o próximo frame (nextFrame)
+            
+            currentPage = nextPage; // Itera sobre as páginas
+            currentFrame = currentPage.textFrames[0];
         }
     }
 }
 
-/*
-
-
-
-SCRIPT ANTIGO
-//// Assuming Every Page has only one main textFrame ---
-
-
-var myDocument = app.documents.item(0);
-var firstPage = myDocument.pages.length-1;
-var myPage = myDocument.pages.item(firstPage);
-
-
-var x = myPage.marginPreferences.left;
-var y = myPage.marginPreferences.top;
-var path = "C:/Users/vinic/Documents/Editora/Digramações/João Benvindo (org) - Semiolinguistica/Arquivos originais/"
-var filename = [
-    "12 - Jaqueline Salviano corrigido.docx",
-    "13 - Jesica Carvalho.docx",
-    "14 - José Magno.docx",
-    "15 - José Maria corrigido.docx",
-    "16 - Luis Felipe atualizado em 27.03.docx",
-    "17 - Maria Juliana corrigido.docx",
-    "18 - Patrícia.docx",
-    "Sobre os autores.docx"
-]
-for(var i = 0; i < filename.length; i++){
-    if(i+firstPage > myDocument.pages.length-1){
-        myDocument.pages.add();
-    }
-    var myPage = myDocument.pages.item(firstPage + i);
-    var myStory = myPage.place(File(path+filename[i]), [x,y], undefined, false,false) [0];
+// Adiciona um Frame a página indicada e depois retorna esse frame
+// Retirado de https://community.adobe.com/t5/indesign-discussions/script-to-autoflow-multiple-text-files/m-p/6214229/highlight/true
+function addFrame(page){
+    var pageMargins = page.marginPreferences;
+    frame = page.textFrames.add();
+    frameTop = page.bounds[0] + pageMargins.top;
+    frameLeft = page.bounds[1] + pageMargins.left;
+    frameBottom = page.bounds[2] - pageMargins.bottom;
+    frameRight = page.bounds[3] - pageMargins.right;
+    frame.geometricBounds = [frameTop, frameLeft, frameBottom, frameRight];
+    return frame;
 }
-
-var myDoc = app.documents[0];
-
-var allPages = myDoc.pages;
-var currentFrame = allPages[firstPage].textFrames[0];
-
-for(var i = 1; i < allPages.length; i++){
-
-    currentFrame.nextTextFrame = allPages[i+firstPage].textFrames[0];
-
-    currentFrame = allPages[i+firstPage].textFrames[0];
-
-    }
-   */
-
-    function alert_scroll (title, input){
-        if (input instanceof Array)
-            input = input.join ("\r");
-        var w = new Window ("dialog", title);
-        var list = w.add ("edittext", undefined, input, {multiline: true, scrolling: true});
-        list.maximumSize.height = w.maximumSize.height-100;
-        list.minimumSize.width = 250;
-        w.add ("button", undefined, "Close", {name: "ok"});
-        w.show ();
-    }
